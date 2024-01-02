6adb397135c133c57572ff334899fbabaa449aaf
juanschcolnik <schcolnikj@gmail.com>
2024-01-02 13:35:03 -0300

    Weights changed.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from 'styled-components/native';
 import { buildProviderTree } from '../utils/BuildProviderTree';
 import { theme } from '../utils/Theme';
 import { Slot } from 'expo-router';
+import { useFonts } from 'expo-font';
 import store from '../redux/store';
 import React from 'react';
 
@@ -12,6 +13,19 @@ const _layout = () => {
     [ThemeProvider, { theme }],
     [Provider, { store }],
   ]);
+
+  const [ fontsLoaded ] = useFonts({
+    'Roboto-Thin': require('../../assets/fonts/Roboto-Thin.ttf'),
+    'Roboto-Light': require('../../assets/fonts/Roboto-Light.ttf'),
+    'Roboto-Regular': require('../../assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Medium': require('../../assets/fonts/Roboto-Medium.ttf'),
+    'Roboto-Bold': require('../../assets/fonts/Roboto-Bold.ttf'),
+    'Roboto-Black': require('../../assets/fonts/Roboto-Black.ttf')
+  })
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <ProvidersTree>
