4f97d4ad7223bfec6c0d6e3cd5f7d8858ae9ea57
valenchus <valentin.morali@sirius.com.ar>
2024-01-04 13:43:30 -0800

    type bottomtab

--- src/app/(tabs)/bottomTabs.tsx
+++ src/app/(tabs)/bottomTabs.tsx
@@ -1,8 +1,19 @@
+import React, { ReactNode } from 'react';
 import CompassIcon from '../../../assets/icons/CompassIcon';
 import BoltIcon from '../../../assets/icons/BoltIcon';
 import PersonCircleIcon from '../../../assets/icons/PersonCircleIcon';
+import { IconInterface } from '../../../assets/icons/types';
 
-export const bottomTabs = [
+interface BottomTab {
+  id: string;
+  name: string;
+  screen: string;
+  iconName: ReactNode;
+  active?: boolean;
+  roles?: string[];
+}
+
+export const bottomTabs: BottomTab[] = [
   {
     id: 'Profile',
     name: 'profile',
