42e2eebc2526376b0104984dc0639f4e66f6d4dd
Juan Schcolnik <schcolnikj@gmail.com>
2024-01-11 17:30:04 -0300

    Update index.tsx

--- src/app/(tabs)/profile/index.tsx
+++ src/app/(tabs)/profile/index.tsx
@@ -3,16 +3,6 @@ import { StyledTextBubble } from '../../../components/styled/TextBubble';
 
 export default function Page() {
   return (
-    <View>
-      <StyledTextBubble type="text" user="professor" isLast={false} content="Hola cómo te va?" />
-      <StyledTextBubble type="text" user="student" isLast={false} content="Muy bien, y a vos?" />
-      <StyledTextBubble
-        type="text"
-        user="professor"
-        isLast={false}
-        content="Bien bien. Empecemos con la clase de hoy. Leiste los textos de tarea?"
-      />
-      <StyledTextBubble type="image" user="student" content="https://via.placeholder.com/300" />
-    </View>
+    <Text>Index page of Profile Tab</Text>
   );
 }
