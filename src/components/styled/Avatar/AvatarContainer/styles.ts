4730808cb2f0f1b2b188d55bdc5718c652ea6755
Juan Schcolnik <schcolnikj@gmail.com>
2024-01-11 17:28:59 -0300

    Update styles.ts

--- src/components/styled/Avatar/AvatarContainer/styles.ts
+++ src/components/styled/Avatar/AvatarContainer/styles.ts
@@ -4,7 +4,7 @@ import { MessageProps } from '../../../../utils/constants';
 export const AvatarContainer = styled.View<MessageProps>`
   width: 28px;
   height: 28px;
-  borderradius: 50px;
+  border-radius: 50px;
   background: ${(props) => {
     if (props.user === 'student') {
       return props.theme.primary200;
