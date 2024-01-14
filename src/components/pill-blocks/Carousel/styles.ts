e0d5112ff020a7410014a2fe5983ff065b2e667e
fabrizioserial <fabrizio.serial@ing.austral.edu.ar>
2024-01-14 13:40:31 -0300

    feat: add customError

--- src/components/pill-blocks/Carousel/styles.ts
+++ src/components/pill-blocks/Carousel/styles.ts
@@ -1,10 +1,10 @@
-import styled from "styled-components";
-import {Dimensions, View} from "react-native";
-import {ScrollView} from "react-native-gesture-handler";
+import styled from 'styled-components';
+import { Dimensions, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export const StyledCarouselContainer = styled(ScrollView)`
-    width: ${Dimensions.get('window').width * 0.95}px;
-    gap: 16px;
-    display: flex;
-    flex-grow: 0;
-`
+  width: ${Dimensions.get('window').width * 0.95}px;
+  gap: 16px;
+  display: flex;
+  flex-grow: 0;
+`;
--- src/react-native-svg.d.ts
+++ src/react-native-svg.d.ts
@@ -1,7 +1,7 @@
-import "react-native-svg"
-declare module "react-native-svg" {
-    export interface SvgProps {
-        xmlns?: string
-        xmlnsXlink?: string
-    }
+import 'react-native-svg';
+declare module 'react-native-svg' {
+  export interface SvgProps {
+    xmlns?: string;
+    xmlnsXlink?: string;
+  }
 }
