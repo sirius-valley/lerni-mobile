8112ef4bfd64d007e5fbb11706a51e356c6290cb
valenchus <valentin.morali@sirius.com.ar>
2024-01-26 15:00:20 -0800

    feature: add pressable functionality

--- src/components/program/ProgramCard/index.tsx
+++ src/components/program/ProgramCard/index.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { useTheme } from 'styled-components/native';
 import { StyledBox, StyledColumn, StyledText } from '../../styled/styles';
 import * as Progress from 'react-native-progress';
-import { setModalOpen } from '../../../redux/slice/utils.slice';
-import { ModalTypeEnum } from '../../../utils/utils';
 import { useDispatch } from 'react-redux';
 import ProgramImage from '../ProgramImage';
 import { Status } from '../../../app/(auth)/(tabs)/explore/utils';
+import { Pressable } from 'react-native';
 
 interface ProgramCardProps {
   id: string;
@@ -23,38 +22,40 @@ const ProgramCard = ({ id, title, imgUrl, status, progress, onPress }: ProgramCa
   const handleOnPress = () => onPress && onPress();
 
   return (
-    <StyledColumn
-      key={id}
-      css={{
-        width: 109,
-        height: 145,
-        gap: 6,
-        alignItems: 'center',
-      }}
-    >
-      <ProgramImage status={status} imgUrl={imgUrl} />
-
-      <StyledText
-        variant="body1"
-        css={{ color: theme.gray100, opacity: status === 'locked' ? 0.3 : 1 }}
+    <Pressable onPress={handleOnPress}>
+      <StyledColumn
+        key={id}
+        css={{
+          width: 109,
+          height: 145,
+          gap: 6,
+          alignItems: 'center',
+        }}
       >
-        {title}
-      </StyledText>
+        <ProgramImage status={status} imgUrl={imgUrl} />
 
-      {status === 'in_progress' && (
-        <StyledBox css={{ width: '100%', gap: 10 }}>
-          <Progress.Bar
-            unfilledColor={theme.gray600}
-            color={theme.primary400}
-            height={6}
-            progress={progress}
-            borderWidth={0}
-            width={null}
-            borderRadius={20}
-          />
-        </StyledBox>
-      )}
-    </StyledColumn>
+        <StyledText
+          variant="body1"
+          css={{ color: theme.gray100, opacity: status === 'locked' ? 0.3 : 1 }}
+        >
+          {title}
+        </StyledText>
+
+        {status === 'in_progress' && (
+          <StyledBox css={{ width: '100%', gap: 10 }}>
+            <Progress.Bar
+              unfilledColor={theme.gray600}
+              color={theme.primary400}
+              height={6}
+              progress={progress}
+              borderWidth={0}
+              width={null}
+              borderRadius={20}
+            />
+          </StyledBox>
+        )}
+      </StyledColumn>
+    </Pressable>
   );
 };
 
