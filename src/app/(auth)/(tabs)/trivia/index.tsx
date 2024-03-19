9347ac7d559f13fa76370318e2fcf278f40f1518
schcolnikj <schcolnikj@gmail.com>
2024-03-19 18:39:00 -0300

    feat: trivia history card

--- src/app/(auth)/(tabs)/trivia/index.tsx
+++ src/app/(auth)/(tabs)/trivia/index.tsx
@@ -1,13 +1,81 @@
 import { useRouter } from 'expo-router';
 import { useEffect } from 'react';
-import { StyledColumn } from '../../../../components/styled/styles';
+import { StyledColumn, StyledRow } from '../../../../components/styled/styles';
 import Button from '../../../../components/styled/Button';
-
+import React from 'react';
+import { TriviaHistoryCard } from '../../../../components/trivia/TriviaHistoryCard';
+import { Pressable } from 'react-native';
 export default function Page() {
   const router = useRouter();
 
+  const user = {
+    firstName: 'Juan',
+    lastName: 'Schcolnik',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/b/bf/Foto_Perfil_.jpg',
+    score: 8,
+  };
+
+  const opponent = {
+    firstName: 'Oli',
+    lastName: 'Sanguinetti',
+    image:
+      'https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos.jpg',
+    score: 7,
+  };
+  const triviahistory = [
+    {
+      user: user,
+      opponent: opponent,
+      triviaId: '1',
+      date: new Date(),
+    },
+    {
+      user: {
+        firstName: 'Juan',
+        lastName: 'Schcolnik',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/b/bf/Foto_Perfil_.jpg',
+        score: 5,
+      },
+      opponent: opponent,
+      triviaId: '2',
+      date: new Date(),
+    },
+    {
+      user: {
+        firstName: 'Juan',
+        lastName: 'Schcolnik',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/b/bf/Foto_Perfil_.jpg',
+        score: 7,
+      },
+      opponent: opponent,
+      triviaId: '3',
+      date: new Date(),
+    },
+  ];
+
   return (
-    <StyledColumn>
+    <StyledColumn
+      css={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 32,
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      {triviahistory.map((card, idx) => (
+        <Pressable
+          onPress={() => alert(`trivia modal ${idx} trivia id: ${card.triviaId}`)}
+          key={idx}
+        >
+          <TriviaHistoryCard
+            user={card.user}
+            opponent={card.opponent}
+            date={card.date}
+            id={card.triviaId}
+          />
+        </Pressable>
+      ))}
       <Button onPress={() => router.push('/(auth)/triviaScreen')}>Trivia</Button>
     </StyledColumn>
   );
